f063d2a557fdc2392237009990c75ad0028e65ea
rowongjr <36941095+rowongjr@users.noreply.github.com>
2019-03-05 17:01:55 -0800

    Add files via upload

---
+++ Dev1/praticejavascript/main.js
@@ -0,0 +1,13 @@
+var input = prompt("Which door is the Gem in on the right or left?"); 
+if (input == "left") {
+    console.log("Sorry wrong door")
+} else if (input == "right") {
+    console.log("You received 50 gems!!!");
+}
+
+var input = prompt("Type yes or no if you enjoyed playing the game.");
+if (input == "yes") {
+    console.log("Glad to hear you enjoyed the game.");
+} else if (input == "no") {
+    console.log("Sorry to hear that you did not enjoy the game.");
+}
